4415f3c99e5f78c031eab84f1aef64a4d912474d
oscarramos12 <47162947+oscarramos12@users.noreply.github.com>
2022-03-07 18:31:37 -0600

    Update HTArboles.py

--- HTArboles.py
+++ HTArboles.py
@@ -1,124 +1,99 @@
-#ARBOL DE CLASIFICACION----------------------------------------------------------------------------------------------------
-
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-
-dataset = pd.read_csv('train2.csv') #ya procesados los datos
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
+import matplotlib.pyplot as plt
+import sklearn.cluster as cluster
+import sklearn.metrics as metrics
+import sklearn.preprocessing
+import scipy.cluster.hierarchy as sch
+import sklearn.mixture as mixture
+import random
+from sklearn.cluster import KMeans, MeanShift, estimate_bandwidth
+from sklearn.preprocessing import StandardScaler, PowerTransformer
+from sklearn.metrics import silhouette_score
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
-
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-
-from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
-classifier.fit(X_train, y_train)
-
-print(classifier.predict(sc.transform([[30,87000]])))
-
-y_pred = classifier.predict(X_test)
-
-
-#Accuracy y matriz de confusion
-from sklearn.metrics import confusion_matrix, accuracy_score
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-accuracy_score(y_test, y_pred)
-
-
-
-#ARBOL DE REGRESION----------------------------------------------------------------------------------------------------
-
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-dataset = pd.read_csv('train2.csv') #ya procesados los datos
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn import tree
+from sklearn import metrics
+from sklearn.model_selection import cross_val_predict
+from sklearn.decomposition import PCA
+from sklearn.metrics import confusion_matrix
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+import matplotlib.cm as cm
+import sys
+import warnings
+import scipy.stats as stats
+import pylab
+from scipy.stats import shapiro
 from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
-regressor.fit(X, y)
-
-regressor.predict([[6.5]])
-
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Decision Tree Regression')
-
-plt.show()
-
-
-
-#ARBOL DE RANDOM FOREST CLASIFICACION----------------------------------------------------------------------------------------------------
-
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-dataset = pd.read_csv('train2.csv') #ya procesados los datos
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-
-
 from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
-classifier.fit(X_train, y_train)
-
-
-print(classifier.predict(sc.transform([[30,87000]])))
-
-y_pred = classifier.predict(X_test)
-
-from sklearn.metrics import confusion_matrix, accuracy_score
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-accuracy_score(y_test, y_pred)
-
-
-
-#ARBOL DE REGRESION RANDOM FOREST----------------------------------------------------------------------------------------------------
-
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-dataset = pd.read_csv('train2.csv') #ya procesados los datos
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
-regressor.fit(X, y)
+from sklearn.datasets import make_classification
+from sklearn.tree import export_graphviz
 
-regressor.predict([[6.5]])
+from mpl_toolkits.mplot3d import Axes3D
+plt.rcParams['figure.figsize'] = (16, 9)
+plt.style.use('ggplot')
 
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Random Forest Regression')
+Data = pd.read_csv('train.csv')
 
-plt.show()
+Test = pd.read_csv('test.csv')
+
+#print(Data)
+
+normal = Data.select_dtypes(include = np.number)
+
+#print(normal.describe())
+
+normal = normal.drop(['Id', 'LowQualFinSF', 'YearBuilt', 'YearRemodAdd', 'GarageYrBlt', 'GarageArea', 'MoSold', 'YrSold', 'MSSubClass', 'OverallCond', 'BsmtFinSF1','BsmtFinSF2', 'BsmtUnfSF', '2ndFlrSF', 'BsmtFullBath', 'BsmtHalfBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'WoodDeckSF', 'EnclosedPorch','3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal', 'LotFrontage', 'LotArea', 'MasVnrArea', '1stFlrSF', '2ndFlrSF', 'TotRmsAbvGrd', 'Fireplaces', 'OpenPorchSF' ], axis = 1)
+
+correlation_mat = normal.corr()
+
+NC = normal.columns.values
+
+SP = correlation_mat.iloc[-1]
+
+SaleP = normal[['SalePrice']]
+
+#print(SP)
+
+#variables = correlation_mat.query("sector == 'SalePrice & ")
+#print(correlation_mat)
+
+
+#sns.heatmap(correlation_mat, annot = True)
+normal = normal.dropna()
+CN = normal.columns.values
+
+#print(CN)
 
 
 
-X_test = sc.transform(X_test)
+
+
+# In[268]:
+
+"""
+n = 0
+while n < 20:
+    for i in CN:
+        normal = normal[(normal[i] < normal[i].mean()+2*(normal[i].std())) & (normal[i] > normal[i].mean()-2*(normal[i].std()))] 
+        n += 1
+
+
+fig = plt.figure()
+g = 0
+for i in CN:
+	plt.subplot(5,3,g+1)
+	sns.distplot(normal[i])
+	plt.xlabel(i)
+	g += 1
+
+"""
+#normal = normal[['OverallQual', 'TotalBsmtSF', 'GrLivArea' ,'FullBath', 'GarageCars', 'SalePrice']]
+H = normal
+X = np.array(normal)
+X.shape
+print('Hopkins', pyclustertend.hopkins(X,len(X)))
